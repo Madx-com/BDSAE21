ab7ce77b4bac580efd31c37217a6b087b7ce6050
malj <malj@itu.dk>
2021-09-09 19:05:51 +0200

    Tested and Implemented Divisible by 4.

--- src/HelloWorld/Program.cs
+++ src/HelloWorld/Program.cs
@@ -6,11 +6,17 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            
+            IsLeapYear(2000);
         }
 
         public static bool IsLeapYear(int year)
         {
+            var check = year % 4;
+            Console.WriteLine(check);
+            if (check == 0)
+            {
+                return true;
+            }
             return false;
         }
     }
