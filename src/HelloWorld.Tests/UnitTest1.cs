f681e06d9cbdfd82a2b721ed1c8eeb9051536dbc
Madx-com <Madx.com@gmail.com>
2021-09-09 18:20:59 +0200

    C# Initializing (#2)
    * Finally Okay Dotnet CLI difficult.. use slides from lecture to rember.
    
    * Added The inclusion for the tests
    
    Co-authored-by: malj <malj@itu.dk>

---
+++ src/HelloWorld.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using HelloWorld;
+
+namespace HelloWorld.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            new Program();
+        }
+    }
+}
