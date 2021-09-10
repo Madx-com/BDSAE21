4b57a582ab6be721df92e455e85eae7bd3b30e8d
malj <malj@itu.dk>
2021-09-10 10:36:51 +0200

    Created user interface for IsLeapYear

--- src/HelloWorld/Program.cs
+++ src/HelloWorld/Program.cs
@@ -4,20 +4,34 @@ namespace HelloWorld
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Console.WriteLine("Enter a year to check if It is a Leap Year:");
+            var input = Console.ReadLine();
+            int inputyear;
+            if(Int32.TryParse(input, out inputyear))
+            {
+                if(IsLeapYear(inputyear))
+                {
+                    Console.WriteLine("yay");
+                }
+                else
+                {
+                    Console.WriteLine("nay");
+                }
+            }
+            // no loop yet
         }
 
         public static bool IsLeapYear(int year)
         {
-            var check = year % 4;
             if (year%4 == 0 )
             {
                 if (year%100 == 0 && year%400 != 0)
                 {
                     return false;
                 }
-                
+
                 return true;
             }
             return false;
